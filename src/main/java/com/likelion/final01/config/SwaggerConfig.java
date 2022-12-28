7d7ab98392c4b71ea6a89f62c9cc6f0176f2e63a
dohyunre702 <dohyunre702@gmail.com>
2022-12-29 00:17:42 +0900

    [Docs] swaggerConfig 수정

--- src/main/java/com/likelion/final01/config/SwaggerConfig.java
+++ src/main/java/com/likelion/final01/config/SwaggerConfig.java
@@ -1,18 +1,15 @@
 package com.likelion.final01.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Configuration
 @EnableSwagger2
@@ -21,34 +18,19 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .consumes(getConsumeContentTypes())
-                .produces(getProduceContentTypes())
                 .apiInfo(getApiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())//현재 RequestMapping으로 할당된 모든 URL 리스트 추출
                 //.apis(RequestHandlerSelectors.basePackage("com.likelion.final01.controller"))
-                .paths(PathSelectors.ant("/member/**")) //그 중 /member/**인 URL들만 필터링
+                //.paths(PathSelectors.ant("/member/**")) //그 중 /member/**인 URL들만 필터링
+                .paths(PathSelectors.any())
                 .build();
     }
-
-    private Set<String> getConsumeContentTypes() {
-        Set<String> consumes = new HashSet<>();
-        consumes.add("application/json;charset=UTF-8");
-        consumes.add("application/x-www-form-urlencoded");
-        return consumes;
-    }
-
-    private Set<String> getProduceContentTypes() {
-        Set<String> produces = new HashSet<>();
-        produces.add("application/json;charset=UTF-8");
-        return produces;
-    }
-
-    private ApiInfo getApiInfo() {
+        private ApiInfo getApiInfo() {
         return new ApiInfoBuilder()
                 .title("파이널미션01")
                 .description("멋사스네스(MutsaSNS) API")
-                //.contact(new Contact("dhlee", "https://github.com/dohyunre702", "dohyunre702@gmail.com"))
+                .contact(new Contact("dhlee", "https://github.com/dohyunre702", "dohyunre702@gmail.com"))
                 .version("1.0")
                 .build();
     }
