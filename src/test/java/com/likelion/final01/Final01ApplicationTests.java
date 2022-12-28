cd7b7dd202c34adf485a6a22cea0f4905416a4f3
dohyunre702 <dohyunre702@gmail.com>
2022-12-28 16:51:49 +0900

    [fix] contextLoads() FAILED error : @SpringBootTest 주석처리

--- src/test/java/com/likelion/final01/Final01ApplicationTests.java
+++ src/test/java/com/likelion/final01/Final01ApplicationTests.java
@@ -3,7 +3,7 @@ package com.likelion.final01;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class Final01ApplicationTests {
 
     @Test
