33b5ab883803a815bb65c15d7d11e32ff054014b
dohyunre702 <dohyunre702@gmail.com>
2022-12-28 16:37:15 +0900

    first commit

---
+++ src/test/java/com/likelion/final01/Final01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.final01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Final01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
