ecd2157c2368bc4ed41327216af9e4f91847e77e
dohyunre702 <dohyunre702@gmail.com>
2022-12-29 10:04:51 +0900

    [add] SumofDigit (Test작성중)

--- src/main/java/com/likelion/final01/controller/HelloController.java
+++ src/main/java/com/likelion/final01/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.likelion.final01.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +12,16 @@ public class HelloController {
     @GetMapping
     public String hello() {
         return "Hello! 200";
+    }
+
+    @GetMapping("{num}")
+    public Integer sumOfDigit(@RequestParam("num") Integer num) {
+        int sum = 0;
+        while (num > 0) {
+          sum +=  num % 10;
+          num /= 10;
+        }
+        return sum;
     }
 
 }
